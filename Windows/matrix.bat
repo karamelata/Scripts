0cf8730015178f82b3ceb57a47820d70d62b459b
Dave Machado <davemachado@users.noreply.github.com>
2016-10-24 23:29:50 -0400

    Create matrix.bat

---
+++ Windows/matrix.bat
@@ -0,0 +1,10 @@
+@ echo off
+color 0a
+:a
+echo %random%%random%%random%
+%random%%random%%random%
+%random%%random%%random%
+%random%%random%%random%
+%random%%random%%random%
+%random%%random%%random%
+goto a
