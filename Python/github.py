16d53c17aed81237b1325e2e60c2d0e3af017ede
Dave Machado <dave@davemachado.com>
2016-10-29 19:59:42 -0400

    Add GitHub User lookup

---
+++ Python/github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import urllib
+import json
+import sys
+
+def getUserDataAPI(user):
+	url='https://api.github.com/users/'+str(user)
+	response = urllib.urlopen(url).read()
+	jsonvalues = json.loads(response)
+	if (jsonvalues):
+		userName = jsonvalues['name']
+		print user + " - " + userName
+		for key, value in jsonvalues.items():
+			print str(key) + " - " + str(value)
+	else:
+		print "User could not be found!"
+
+
+if len(sys.argv) > 1:
+	user = str(sys.argv[1])
+	getUserDataAPI(user)
+else:
+	print ("syntax: " + sys.argv[0] + " <username>")
