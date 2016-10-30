a15433e3c65ffccc829a2c3c5472e5db3f7b3b43
Dave Machado <dave@davemachado.com>
2016-10-30 16:00:03 -0400

    Update logging and function breakdown

--- Python/plex.py
+++ Python/plex.py
@@ -4,38 +4,51 @@ import sys
 import urllib
 import json
 
-import xlsxwriter
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 from collections import namedtuple
 Movie = namedtuple("Movie", "title director genre time rating imdbMeter rottenMeter rottenUserMeter releaseYear boxOffice plot rottenURL")
+ROGUE = '-'
 
-movieArr = []
-goodFile = open("success.txt", "w")
-badFile = open("failure.txt", "w")
+class Logger:
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("output.log", "a")
 
-def writeToSS(movieArr = []):
+    def __init__(self, fileName):
+        self.terminal = sys.stdout
+        self.log = open(fileName, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass
+
+def openSS(sheetName):
 	scope = ['https://spreadsheets.google.com/feeds']
 	credentials = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
 	gc = gspread.authorize(credentials)
+	wks = gc.open(sheetName).sheet1
+	return wks
 
-	wks = gc.open("Plex").sheet1
-	#row = 1
-	#NUM_OF_MOVIE_VALUES = 12
-
-	for movie in movieArr:
-		#for x in range(NUM_OF_MOVIE_VALUES):
-		title = movie[0]
-		try:
-			wks.find(title)
-			print "Skipping: " + title + "\n"
- 		except gspread.exceptions.CellNotFound:
-			print "Adding: " + title + "\n"
-			wks.append_row(movie)
-			#wks.update_cell(row, x+1, movie[x])
-		#row = row + 1
-	wks.close()
+def writeToSS(wks, movie):
+	title = movie[0]
+	apiResponse = movie[1]
+	try:
+		wks.find(title)
+		print "Already in workbook: " + title + "\n"
+	except gspread.exceptions.CellNotFound:
+		if apiResponse is ROGUE:
+			print "Adding raw title to workbook:" + title + "\n"
+		else:
+			print "Adding to workbook: " + title + "\n"
+		wks.append_row(movie)
 
 def getMovie(title):
 	url='http://www.omdbapi.com/?y=&plot=short&r=json&tomatoes=true&t='+str(title)
@@ -55,10 +68,11 @@ def getMovie(title):
 		plot = jsonvalues['Plot']
 		tomatoURL = jsonvalues['tomatoURL']
 		movie = Movie(title, director, genre, time, rated, imdb, tomatoScore, tomatoUser, year, boxOffice, plot, tomatoURL)
-		movieArr.append(movie)
-		print title + " processed." + "\n"
+		print title + " processed."
 	else:
-		print title + " failed!" + "\n"
+		movie = Movie(title, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE, ROGUE)
+		print title + " failed!"
+	return movie
 
 def main():
 	if len(sys.argv) > 1:
@@ -67,16 +81,19 @@ def main():
 		print "Invalid file path!"
 		sys.exit()
 
+	sys.stdout = Logger("plex-data.log")
+	worksheet = openSS("Plex Data")
+
 	dirlist = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
 	numOfDir = len(dirlist)
 	for x in range(numOfDir):
 		title = dirlist[x]
 		title = title.replace('', '')
-		print  str(x+1) + "/" + str(numOfDir) + " Processing " + title + "...\n"
-		getMovie(title)
-	writeToSS(movieArr)
-	goodFile.close()
-	badFile.close()
+		print str(x+1) + "/" + str(numOfDir)
+		print  "Processing " + title + "..."
+		movie = getMovie(title)
+		writeToSS(worksheet, movie)
+	worksheet.close()
 
 if __name__ == '__main__':
     main()
