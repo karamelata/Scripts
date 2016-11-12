328a45140a3658b4f04f1a9b7d14f42cc930f250
Dave Machado <dave@davemachado.com>
2016-11-11 19:44:44 -0500

    Update Movie Searching Printing

--- Python/movie.py
+++ Python/movie.py
@@ -2,17 +2,27 @@
 import urllib
 import json
 import sys
+import os
+import platform
+
+def clearScreen():
+	if (platform.system() == "Windows"):
+		os.system('cls')
+	else:
+		os.system('clear')
 
 def getMovie(title):
 	url='http://www.omdbapi.com/?y=&plot=short&r=json&tomatoes=true&t='+str(title)
 	response = urllib.urlopen(url).read()
 	jsonvalues = json.loads(response)
 	if jsonvalues["Response"]=="True":
+		clearScreen()
 		title = jsonvalues['Title']
 		imdbrating = jsonvalues['imdbRating']
-		print title + " - " + imdbrating
+		tomatoScore = jsonvalues['tomatoMeter']
+		print "--------------------------- " + title + " - " + imdbrating + "," + tomatoScore + " ---------------------------" + "\n"
 		for key, value in jsonvalues.items():
-			print key + " - " + value
+			print key + ": " + value
 	else:
 		print "Movie could not be found!"
 
