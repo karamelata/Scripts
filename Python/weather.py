c6f117162390a2bbe95cae78f4a74744bcb1ce15
Dave Machado <dave@davemachado.com>
2016-11-23 23:50:15 -0500

    Add Geocoding support through GeoPy

--- Python/weather.py
+++ Python/weather.py
@@ -8,6 +8,7 @@ import time
 import datetime
 import urllib
 import json
+from geopy.geocoders import Nominatim
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -17,6 +18,9 @@ Weather = namedtuple("Weather", "timestamp year month day weekDay condition minT
 ROGUE = '-'
 
 ONE_DAY = 86400
+DEFAULT_COOR = "42.6751,-71.4828"
+LOCATION_COOR = "NULL"
+LOCATION_INFO = "NULL"
 
 API_KEY = open('darkSky_api.txt', 'r').readline()
 
@@ -26,6 +30,9 @@ def clearScreen():
 	else:
 		os.system('clear')
 
+def pressKeyToContinue():
+	raw_input("Press Enter to continue...")
+
 def openSS(bookName, sheetName):
 	scope = ['https://spreadsheets.google.com/feeds']
 	credentials = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
@@ -33,20 +40,42 @@ def openSS(bookName, sheetName):
 	wks = gc.open(bookName).worksheet(sheetName)
 	return wks
 
+def getCoordinates():
+	geolocator = Nominatim()
+	zipStr = str(raw_input("Zip Code: "))
+	if len(zipStr) < 5:
+		# Python does not read leading zeros, so pad an extra '0'
+		# if x < 5 (length of zip codes)
+		zipStr.zfill(5)
+	zipStr += " United States"
+	#location = geolocator.reverse("42.6751,-71.4828")
+	location = geolocator.geocode(zipStr)
+	x = str(location.latitude)
+	y = str(location.longitude)
+	z = x + ", " + y
+	print("Coordinates: " + z)
+	return z
+
+def getLocationInfo(coordinates):
+	geolocator = Nominatim()
+	location = geolocator.reverse(coordinates)
+	return location.address
+
 def getWeather(when):
+	global LOCATION_COOR
+	global LOCATION_INFO
 	if (when == 0):
-		url='https://api.darksky.net/forecast/' + API_KEY + '/42.6751,-71.4828'
+		url='https://api.darksky.net/forecast/' + API_KEY + '/' + LOCATION_COOR
 		weatherTime = datetime.datetime.now()
-		print("---- Weather for " + weatherTime.strftime('%m-%d-%Y') + " ----")
 	# If unix timestamp is passed, use Time Machine API call
 	else:
-		url='https://api.darksky.net/forecast/' + API_KEY + '/42.6751,-71.4828,' + str(when) + '?exclude=flags'
+		url='https://api.darksky.net/forecast/' + API_KEY + '/' + LOCATION_COOR + ',' + str(when) + '?exclude=flags'
 		weatherTime = datetime.datetime.fromtimestamp(when)
-		print("---- Weather for " + weatherTime.strftime('%B %d, %Y (%a)') + " ----")
-
+	print("---- Weather for " + weatherTime.strftime('%B %d, %Y (%a)') + " ----")
+	
 	response = urllib.urlopen(url).read()
 	jsonvalues = json.loads(response)
-	if jsonvalues["timezone"]=="America/New_York":
+	if 'timezone' in jsonvalues:
 
 		if 'summary' not in jsonvalues['daily']['data'][0]:
 		    summary = ROGUE
@@ -109,55 +138,78 @@ def getWeather(when):
 		sys.exit()
 	return result
 
-def getInteractiveChoice():
-	while True:
-		clearScreen()
-		print("-- WEATHER DATA --")
-		print("Please choose from the following options.")
-		print("(1) Today's Weather")
-		print("(2) Tomorrow's Weather")
-		print("(3) Yesterday's Weather")
-		print("(4) Past Weather from Time Machine")
-		try:
-			choice = int(raw_input("Choice: "))
-		except ValueError:
-			continue
-		if choice < 1 or choice > 4:
-			continue
-		else:
-			return choice
-
-def userMode(choice):
-	if(choice == 0):
-		choice = getInteractiveChoice()
-	if choice == 1:
-		result = getWeather(0)
-		timestamp = datetime.datetime.now()
-	if choice == 2:
-		timestamp = int(time.time())
-		timestamp += ONE_DAY
-		result = getWeather(timestamp)
-	if choice == 3:
-		timestamp = int(time.time())
-		timestamp -= ONE_DAY
-		result = getWeather(timestamp)
-	if choice == 4:
-		print("-- Time Machine Weather Report --")
-		dateStr = raw_input("MM/DD/YYYY: ")
-		try:
-			dateStr += " 11:00PM"
-			timestamp = datetime.datetime.strptime(dateStr, "%m/%d/%Y %I:%M%p")
-			unixTime = time.mktime(timestamp.timetuple())
-		except ValueError:
-			raise ValueError("Incorrect data format, should be MM/DD/YYYY")
-		result = getWeather(int(unixTime))
+def printReport(result):
 	print("Summary: " + str(result[5]))
 	print("Low: " + str(result[6]))
 	print("High: " + str(result[7]))
 	# if precipProb > 0
 	if(result[8] > 0):
 		print("Chance of " + str(result[9]) + ": " + str(result[8]))
+	pressKeyToContinue()
 
+def getInteractiveChoice():
+	global LOCATION_COOR
+	global LOCATION_INFO
+	while True:
+		clearScreen()
+		print("-- WEATHER DATA --")
+		print("Location: " + LOCATION_INFO + ' (' + LOCATION_COOR + ')')
+		print("Please choose from the following options:")
+		print("(1) Today's Weather")
+		print("(2) Tomorrow's Weather")
+		print("(3) Yesterday's Weather")
+		print("(4) Past Weather from Time Machine")
+		print("(5) Change Location")
+		print("(0) Quit")
+		try:
+			choice = int(raw_input("Choice: "))
+		except ValueError:
+			continue
+		if choice < 0 or choice > 5:
+			continue
+		else:
+			return choice
+
+def userMode(choice):
+	global LOCATION_COOR
+	global LOCATION_INFO
+	while True:
+		if choice == -1:
+			choice = getInteractiveChoice()
+		if choice == 0:
+			sys.exit()
+		if choice == 1:
+			result = getWeather(0)
+			timestamp = datetime.datetime.now()
+			printReport(result)
+		if choice == 2:
+			timestamp = int(time.time())
+			timestamp += ONE_DAY
+			result = getWeather(timestamp)
+			printReport(result)
+		if choice == 3:
+			timestamp = int(time.time())
+			timestamp -= ONE_DAY
+			result = getWeather(timestamp)
+			printReport(result)
+		if choice == 4:
+			print("-- Time Machine Weather Report --")
+			dateStr = raw_input("MM/DD/YYYY: ")
+			try:
+				dateStr += " 11:00PM"
+				timestamp = datetime.datetime.strptime(dateStr, "%m/%d/%Y %I:%M%p")
+				unixTime = time.mktime(timestamp.timetuple())
+			except ValueError:
+				raise ValueError("Incorrect data format, should be MM/DD/YYYY")
+			result = getWeather(int(unixTime))
+			printReport(result)
+
+		if choice == 5:
+			weatherCoordinates = getCoordinates()
+			weatherLocation = getLocationInfo(weatherCoordinates)
+			LOCATION_COOR = weatherCoordinates
+			LOCATION_INFO = weatherLocation
+		choice = -1
 
 def callHelp():
 	print("-- WEATHER DATA --")
@@ -178,12 +230,16 @@ def autoMode():
 	wks.append_row(result)
 
 def main():
+	global LOCATION_COOR
+	global LOCATION_INFO
+	LOCATION_COOR = DEFAULT_COOR
+	LOCATION_INFO = getLocationInfo(LOCATION_COOR)
 	if len(sys.argv) > 1:
 		mode = str(sys.argv[1])
 		if mode == '-a':
 			autoMode()
 		if mode == '-i':
-			userMode(0)
+			userMode(-1)
 		if mode == '-n':
 			userMode(1)
 		if mode == '-t':
@@ -195,8 +251,7 @@ def main():
 		if mode == '-h' or mode == "-help":
 			callHelp()
 	else:
-		print "Invalid execution, try \'" + str(sys.argv[0]) + " -help\'"
-		sys.exit()
+		userMode(-1)
 
 if __name__ == '__main__':
     main()
