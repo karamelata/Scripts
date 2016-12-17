5678a4e923316d1f3c0bcd7425052ec64b543f6c
Dave Machado <dave@davemachado.com>
2016-12-17 18:29:23 -0500

    Update 404 error from API

--- Python/github.py
+++ Python/github.py
@@ -7,14 +7,13 @@ def getUserDataAPI(user):
 	url='https://api.github.com/users/'+str(user)
 	response = urllib.urlopen(url).read()
 	jsonvalues = json.loads(response)
-	if (jsonvalues):
+	if ('login' in jsonvalues):
 		userName = jsonvalues['name']
 		print user + " - " + userName
 		for key, value in jsonvalues.items():
 			print str(key) + " - " + str(value)
 	else:
 		print "User could not be found!"
-
 
 if len(sys.argv) > 1:
 	user = str(sys.argv[1])
