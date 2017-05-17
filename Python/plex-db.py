c5b55a0e2761bb4e9780e14e86a077c9a00762a5
Dave Machado <dave@davemachado.com>
2017-05-17 18:30:07 -0400

    Add Added and Updated timestamps to table structures

--- Python/plex-db.py
+++ Python/plex-db.py
@@ -4,6 +4,7 @@ from getpass import getpass
 import pymysql
 import sys
 import os
+import datetime
 
 FAIL_FLAG = False
 
@@ -33,13 +34,13 @@ def check_in_table(id_to_check, table_name):
 
 
 def insert_movie(x):
-    sql = "INSERT INTO movies (plex_id, title, director, genre, studio, duration_min, duration_ms, rating, score, year, summary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO movies (plex_id, title, director, genre, studio, duration_min, duration_ms, rating, score, year, summary, added, updated) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     try:
         genre = ""
         for e in x.genres:
             genre += e.tag + ','
         args = (x.ratingKey, x.titleSort.encode('utf-8'), x.directors[0].tag.encode('utf-8'), genre[:-1], str(
-                x.studio), x.duration / 60000, x.duration, str(x.contentRating), x.rating, x.year, x.summary.encode('utf-8'))
+                x.studio), x.duration / 60000, x.duration, str(x.contentRating), x.rating, x.year, x.summary.encode('utf-8'), x.addedAt.strftime('%Y-%m-%d %H:%M:%S'), x.updatedAt.strftime('%Y-%m-%d %H:%M:%S'))
         cur.execute(sql, args)
         # Commit your changes in the database
         print("Inserted MV: %s" % (x.titleSort))
@@ -69,6 +70,8 @@ def update_movies():
       score           float(8) NOT NULL,
       year            int NOT NULL,
       summary         text NOT NULL,
+      added             datetime NOT NULL,
+      updated           datetime NOT NULL,
       PRIMARY KEY     (id)
     );
     """
@@ -95,13 +98,13 @@ def update_movies():
 
 
 def insert_show(x):
-    sql = "INSERT INTO tv_shows (plex_id, title, seasons, episodes, rating, genre, studio, score, year, summary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO tv_shows (plex_id, title, seasons, episodes, rating, genre, studio, score, year, summary, added, updated) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     try:
         genre = ""
         for e in x.genres:
             genre += e.tag + ','
         args = (x.ratingKey, x.titleSort.encode('utf-8'), len(x.seasons()), len(x.episodes()),
-                str(x.contentRating), genre[:-1], x.studio, x.rating, x.year, x.summary.encode('utf-8'))
+                str(x.contentRating), genre[:-1], x.studio, x.rating, x.year, x.summary.encode('utf-8'), x.addedAt.strftime('%Y-%m-%d %H:%M:%S'), x.updatedAt.strftime('%Y-%m-%d %H:%M:%S'))
         cur.execute(sql, args)
         # Commit your changes in the database
         print("Inserted SH: %s" % (x.titleSort))
@@ -129,6 +132,8 @@ def update_tv_shows():
       score           float(8) NOT NULL,
       year            int NOT NULL,
       summary         text NOT NULL,
+      added            datetime NOT NULL,
+      updated           datetime NOT NULL,
       PRIMARY KEY     (id)
     );
     """
@@ -156,13 +161,14 @@ def update_tv_shows():
 
 
 def insert_episode(x):
-    sql = "INSERT INTO tv_episodes (plex_id, title, season, ep_index, show_id, duration_min, duration_ms, rating, score, year, summary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO tv_episodes (plex_id, title, season, ep_index, show_id, duration_min, duration_ms, rating, score, year, summary, added, updated) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     try:
         args = (x.ratingKey, x.titleSort.encode('utf-8'), x.parentIndex, x.index,
-                x.grandparentRatingKey, x.duration / 60000, x.duration, x.contentRating, x.rating, x.year, x.summary.encode('utf-8'))
+                x.grandparentRatingKey, x.duration / 60000, x.duration, x.contentRating, x.rating, x.year, x.summary.encode('utf-8'), x.addedAt.strftime('%Y-%m-%d %H:%M:%S'), x.updatedAt.strftime('%Y-%m-%d %H:%M:%S'))
         cur.execute(sql, args)
         # Commit your changes in the database
         print("Inserted EP (%s): %s" % (x.grandparentTitle, x.titleSort))
+        #print(datetime.datetime.fromtimestamp(1284286794).strftime('%Y-%m-%d %H:%M:%S'))
     except pymysql.ProgrammingError as e:
         # Rollback in case there is any error
         db.rollback()
@@ -189,6 +195,8 @@ def update_tv_episodes(episodes):
       score           float(8) DEFAULT 0,
       year            int DEFAULT 0,
       summary         text NOT NULL,
+      added             datetime NOT NULL,
+      updated             datetime NOT NULL,
       PRIMARY KEY     (id)
     );
     """
